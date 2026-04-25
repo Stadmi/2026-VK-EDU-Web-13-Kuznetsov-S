e5bbd03682459c66b96b28ccaadc3810733f526a
Stanislav Kuznetsov <kuzstadmi2006@gmail.com>
2026-04-25 17:00:26 +0300

    Fix pagination: handle PageNotAnInteger in paginate()

--- questions/utils.py
+++ questions/utils.py
@@ -5,13 +5,9 @@ def paginate(objects_list, request, per_page=10):
     paginator = Paginator(objects_list, per_page)
     page_number = request.GET.get('page', 1)
     try:
-        page_number = int(page_number)
-        if page_number < 1:
-            page_number = 1
-    except ValueError:
-        page_number = 1
-    try:
         page = paginator.page(page_number)
+    except PageNotAnInteger:
+        page = paginator.page(1)
     except EmptyPage:
         page = paginator.page(paginator.num_pages)
     return page
